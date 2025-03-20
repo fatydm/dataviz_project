10676df899fa74fb2a4cc8979efc35287f577dd9
rchouhani <rchouhani@me.com>
2025-03-20 12:24:31 +0100

    ajouts des boutons par pays

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -16,11 +16,150 @@ async function obtenerRecetasPorArea(area) {
     }
 }
 
-const armeniaButton = document.getElementById("MX");
-armeniaButton.addEventListener("click", function() {
-        (obtenerRecetasPorArea("British"))
-    })
+const mexicanButton = document.getElementById("MX")
+mexicanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Mexican"))
+})
 
+const americanButton = document.getElementsByClassName("United States")
+americanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("American"))
+})
+
+const britishButton = document.getElementsByClassName("United Kingdom")
+britishButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("British"))
+})
+
+const canadaButton = document.getElementsByClassName("Canada")
+canadaButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Canadian"))
+})
+
+const chineseButton = document.getElementsByClassName("China")
+chineseButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Chinese"))
+})
+
+const croatianButton = document.getElementsById("HR")
+croatianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Croatian"))
+})
+
+const dutchButton = document.getElementsById("NL")
+dutchButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Dutch"))
+})
+
+const egyptianButton = document.getElementsById("EG")
+egyptianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Egyptian"))
+})
+
+const filipinoButton = document.getElementsByClassName("Philippines")
+filipinoButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Filipino"))
+})
+
+const frenchButton = document.getElementsByClassName("France")
+frenchButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("French"))
+})
+
+const greeceButton = document.getElementsByClassName("Greece")
+greeceButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Greek"))
+})
+
+const indianButton = document.getElementsById("IN")
+indianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Indian"))
+})
+
+const irishButton = document.getElementsById("IE")
+irishButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Irish"))
+})
+
+const italianButton = document.getElementsByClassName("Italy")
+italianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Italian"))
+})
+
+const jamaicanButton = document.getElementsById("JM")
+jamaicanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Jamaican"))
+})
+
+const japanButton = document.getElementsByClassName("Japan")
+japanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Japanese"))
+})
+
+const kenyanButton = document.getElementsById("KE")
+kenyanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Kenyan"))
+})
+
+const malaysianButton = document.getElementsByClassName("Malaysia")
+malaysianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Malaysian"))
+})
+
+const moroccanButton = document.getElementsById("MA")
+moroccanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Moroccan"))
+})
+
+const polishButton = document.getElementsById("PL")
+polishButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Polish"))
+})
+
+const portugueseButton = document.getElementsById("PT")
+portugueseButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Portuguese"))
+})
+
+const russianButton = document.getElementsByClassName("Russian Federation")
+russianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Russian"))
+})
+
+const spanishButton = document.getElementsById("ES")
+spanishButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Spanish"))
+})
+
+const thaiButton = document.getElementsById("TH")
+thaiButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Thai"))
+})
+
+const tunisianButton = document.getElementsById("TN")
+tunisianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Tunisian"))
+})
+
+const turkishButton = document.getElementsByClassName("Turkey")
+turkishButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Turkish"))
+})
+
+const ukrainianButton = document.getElementsById("UA")
+ukrainianButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Ukrainian"))
+})
+
+const uruguayanButton = document.getElementsById("UY")
+uruguayanButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Uruguayan"))
+})
+
+const vietnameseButton = document.getElementsById("VN")
+vietnameseButton.addEventListener("click", function() {
+        (obtenerRecetasPorArea("Vietnamese"))
+})
 
 
 async function rechercherChansons(genre, pays, cleApi) {
@@ -60,7 +199,3 @@ async function rechercherChansons(genre, pays, cleApi) {
 // const pays = 'France'; // ou 'USA', 'UK', etc.
 // const maCleApi = 'AIzaSyAx7Fx7yCkS28Kpz48rdCeRG8G68RJfC1E'; // Remplacez par votre clé API
 // afficherChansons(genreMusical, pays, maCleApi);
-
-
-
-
