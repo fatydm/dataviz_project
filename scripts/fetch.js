efcb611a5cb3327d683e10eb2ee75c0d0bdee7fb
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-03-24 14:29:33 +0100

    ajout images et new dossier

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -280,9 +280,6 @@ function updateSize() {
 
 
 
-
-
-
 // Correspondance entre les pays dans le SVG et les aires dans l'API
 const areas = {
         "United States": "American",
@@ -337,8 +334,8 @@ const areas = {
             data.meals.forEach((meal) => { 
                  const recipeDiv = document.createElement("div");
                  recipeDiv.innerHTML = `
+                    <img class="meal-image" src="${meal.strMealThumb}" alt="${meal.strMeal}"> 
                     <h3 class="meal-title">${meal.strMeal}</h3>
-                    <img class="meal-image" src="${meal.strMealThumb}" alt="${meal.strMeal}">
                 `
                 divRecipes.appendChild(recipeDiv);
                 ;
@@ -349,47 +346,3 @@ const areas = {
         }
     }
     
-
-
-
-    
-   
-    
-
-// async function rechercherChansons(genre, pays, cleApi) {
-//     const requete = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${genre} music ${pays}&type=video&key=${cleApi}&maxResults=5`);
-//     const reponse = await requete.json();
-
-//     if (reponse.items) {
-//         return reponse.items.map((item) => ({
-//             // map va a buscar cada letra en los objetos para ver si corresponde
-//             titre: item.snippet.title.slice(0, 20),
-//             miniature: item.snippet.thumbnails.default.url,
-//             link: `https://www.youtube.com/watch?v=$${item.id.videoId}`, 
-//         }));
-//     } else {
-//         return [];
-//     }
-// }
-
-async function afficherChansons(genre, pays, cleApi) {
-    const chansons = await rechercherChansons(genre, pays, cleApi);
-    if (chansons.length > 0) {
-        chansons.forEach((chanson) => {
-            const musica = document.createElement("div");
-            musica.innerHTML = `
-        <h4>${chanson.titre}</h4>
-        <a href="${chanson.url}" target="_blank">Voir la vidéo</a>
-        <img src="${chanson.miniature}" alt="Miniature de la vidéo">
-         `;
-          body.appendChild(musica);
-        });
-    } else {
-        console.log(`Aucune chanson ${genre} trouvée en ${pays}.`);
-    }
-}
-// // Exemple d'utilisation
-// const genreMusical = 'pop'; // ou 'rock', 'hip-hop', etc.
-// const pays = 'France'; // ou 'USA', 'UK', etc.
-// const maCleApi = 'AIzaSyAx7Fx7yCkS28Kpz48rdCeRG8G68RJfC1E'; // Remplacez par votre clé API
-// afficherChansons(genreMusical, pays, maCleApi);
