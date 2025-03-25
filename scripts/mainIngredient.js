7fa664514e159e10b5cdee47c3335971ca79c305
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-03-25 14:36:27 +0100

    modif des mains Ingredients

--- scripts/mainIngredient.js
+++ scripts/mainIngredient.js
@@ -6,7 +6,9 @@ async function getRecipesByIngredients(type) {
     const response = await fetch(url);
     const data = await response.json();
     console.log("Données reçues : ", data);
+
     return data.meals;
+
 }
 getRecipesByIngredients("chicken");
 
@@ -15,33 +17,39 @@ console.log(imagesOfFood);
 
 for (let i = 0; i < imagesOfFood.length; i++) {
     imagesOfFood[i].addEventListener('click', async (event) => {
+        
         console.log('coco. ', event.target.id);
-
+        
         const type = event.target.id;
         const meals = await getRecipesByIngredients(type);
-
+        
         getRecipesByIngredients(event.target.id);
-
+        
         const imagesDiv = document.querySelector('.images');
+
         const divRecipes = document.createElement("div");
         divRecipes.classList.add("recipes-container");
-        divRecipes.innerHTML = `
-            <h2>${meals.length} recettes au ${type}</h2>
-            `
+        
         imagesDiv.parentNode.insertBefore(divRecipes, imagesDiv.nextSibling)
-
+        
+        divRecipes.innerHTML = "";
+        
         if (meals) {
-           meals.forEach((meal) => {
-            
+            meals.forEach((meal) => {
+                
                 const recipeDiv = document.createElement("div");
+                recipeDiv.classList.add("recipe");
+
+                
                 recipeDiv.innerHTML = `
-               <img class="meal-image" src="${meal.strMealThumb}" alt="${meal.strMeal}"> 
-               <h3 class="meal-title">${meal.strMeal}</h3>
-           `
+                <img class="meal-image" src="${meal.strMealThumb}" alt="${meal.strMeal}"> 
+                <h3 class="meal-title">${meal.strMeal}</h3>
+                `
                 divRecipes.appendChild(recipeDiv);
-                ;
             });
-        }
-        recipeDiv.innerHTML = "";
+        
+        } 
+        
     });
 }
+
